fdd37a6c6d3519d9efdc4efed793476754d762c9
ben author <maccruiskeen@gmail.com>
2012-10-22 01:44:43 -0400

    Metacircular improvements.

--- js/metacircular/metacircular.js
+++ js/metacircular/metacircular.js
@@ -1,7 +1,77 @@
-//The SICP metacircular evaluator
-//(but in javascript)
+/**
+ * The SICP metacircular evaluator
+ * (in javascript -- so it's no longer really metacircular at all.)
+ */
+
+/*jslint evil: true, nomen: true*/
+"use strict";
 
 var scheme = scheme || {};
+scheme.VERSION = '0.0.1';
+
+/**
+ * Scheme's environment needs someplace to live.
+ */
+scheme.env = {};
+
+/**
+ * Take a string of scheme and turn it into a JS list.
+ * 
+ * Since almost all scheme syntax is nested lists, we can pretty easily
+ * transform it to Javascript native arrays.
+ *
+ * (+ 1 2 (* 3 4)) becomes ['+', '1', '2', ['*', '3', '4']]
+ *
+ * TODO is this the place to transform `(1 2 3) into (quote 1 2 3)
+ */
+scheme.to_list = function (exp) {
+    var tokens, helper;
+    //First, explode the string (add all allowed chars here TODO!)
+    tokens = exp.match(/[\(\)]|[0-9+a-z+A-Z\+\-\\\']+/g);
+
+    //Then recurse through
+    helper = function () {
+        var token,
+            p = [],
+            i = 0;
+        while (token != ')'){
+            token = tokens.shift();
+            if (token === '('){
+                p.push(helper());
+            } else if (token !== ')'){
+                p.push(token);
+            }
+            //avoid infinite loops
+            if (i>100){
+                throw new scheme.ParseException("Mismatched Parens!", exp);
+            }
+            i++;
+        }
+        return p;
+    };
+    if (tokens[0] === '(') {
+        tokens.shift()
+        return helper();
+    } else {
+        //TODO self-evaluating should be allowed, tight?
+        return tokens;
+        // throw new this.ParseException("Unable to parse string", exp);
+    }
+};
+
+/**
+ * Eval a string containing a scheme expression
+ */
+scheme.evals = function (exp) {
+    return this._eval(this.to_list(exp));
+};
+
+/**
+ * Evaluates a Scheme expression
+ *
+ * From here on out we're assuming we'll get scheme expressions in the form
+ * of native javascript arrays, i.e. ['+', '2', '4'] is equivalent to (+ 2 4).
+ */
 
 // (define (eval exp env)
 //   (cond ((self-evaluating? exp) exp)
@@ -22,8 +92,7 @@ var scheme = scheme || {};
 //                 (list-of-values (operands exp) env)))
 //         (else
 //          (error "Unknown expression type -- EVAL" exp))))
-
-scheme.eval = function (exp, env) {
+scheme._eval = function (exp, env) {
     if (this.is_self_evaluating(exp)) {
         return exp;
     } else if (this.is_variable(exp)) {
@@ -43,63 +112,98 @@ scheme.eval = function (exp, env) {
     } else if (this.is_begin(exp)) {
         this.eval_sequence(begin_actions(exp), env);
     } else if (this.is_cond(exp)) {
-        this.eval(cond_to_if(exp), env);
+        this._eval(cond_to_if(exp), env);
     } else if (this.is_application(exp)) {
-        this.apply(eval(operator(exp), env),
+        this.apply(this._eval(operator(exp), env),
               this.list_of_values(operands(exp), env));
     } else {
         throw new this.EvalException("Unknown expression type", exp);
     }
 };
 
-// (define (self-evaluating? exp)
-//   (cond ((number? exp) true)
-//         ((string? exp) true)
-//         (else false)))
-scheme.is_self_evaluating = function (exp) {
-    //we can get a number out of it
-    if (parseFloat(exp) !== NaN) { 
-        return true;
-    //it's quoted
-    } else if (exp.slice(0, 1) == '"' && exp.slice(-1) == '"'){
+/**
+ * Is it a tagged list?
+ * 
+ * The tagged list is how we're going to identify many special forms.
+ * A quote is tagged with 'quote', a string with 'string', and so on.
+ */
+// (define (tagged-list? exp tag)
+//   (if (pair? exp)
+//       (eq? (car exp) tag)
+//       false))
+scheme.is_tagged_list = function (exp, tag) {
+    if (exp[0] === tag) {
         return true;
     } else {
         return false;
     }
 };
 
-//(define (variable? exp) (symbol? exp))
-// we need a place to keep scheme's environment
-scheme.global = {}
-scheme.is_variable = function (exp) {
-    if (this.global.hasOwnProperty(exp)) {
+/**
+ * CAR and CDR are useful scheme idioms.
+ */
+scheme.car = function (a_list) {
+    return a_list[0]
+}
+scheme.cdr = function (a_list) {
+    return a_list[1]
+}
+
+/**
+ * Is it self evaluating?
+ *
+ * Certain Scheme constructs evaluate themselves; numbers, strings.
+ */
+// (define (self-evaluating? exp)
+//   (cond ((number? exp) true)
+//         ((string? exp) true)
+//         (else false)))
+scheme.is_self_evaluating = function (exp) {
+    if (typeof exp === "number") {
+        return true;
+    } else if (this.is_tagged_list(exp, 'string')) {
         return true;
     } else {
         return false;
     }
-}
+};
 
+/**
+ * Is it a variable?
+ *
+ * Look and see if the name is on the environment.
+ */
+//(define (variable? exp) (symbol? exp))
+scheme.is_variable = function (exp) {
+    if (this.env.hasOwnProperty(exp)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+/**
+ * Is it quoted?
+ *
+ * This may need work an my understanding evolves... TODO.
+ */
 // (define (quoted? exp)
 //   (tagged-list? exp 'quote))
 scheme.is_quoted = function (exp) {
     return this.is_tagged_list(exp, 'quote');
-}
+};
+
+/**
+ * Return the text of a quotation.
+ */
 // (define (text-of-quotation exp) (cadr exp))
 scheme.text_of_quotation = function (exp) {
-    return exp.split(" ").slice(1).join(" ").slice(0, -1)
+    return exp[1]
 }
-// (define (tagged-list? exp tag)
-//   (if (pair? exp)
-//       (eq? (car exp) tag)
-//       false))
-scheme.is_tagged_list = function (exp, tag) {
-    if (typeof(exp) === 'string'
-        && exp.split(" ").slice(0,1).toString() === "(" + tag){
-            return true;
-    } else {
-        return false;
-    }
-}
+
+
+
+
 // (define (assignment? exp)
 //   (tagged-list? exp 'set!))
 // (define (assignment-variable exp) (cadr exp))
@@ -157,7 +261,10 @@ scheme.definition_variable = function (exp) {
 }
 scheme.definition_value = function (exp) {
     var second = exp.split(" ").slice(1,2).toString();
-    if (second.slice(0,1) === "(") {
+    console.log(second);
+    if (second === "(lambda") {
+        return scheme.cdr(scheme.cdr(exp))
+    } else if (second.slice(0,1) === "(") {
         return this.make_lambda(
             //two difficult regexes here to do cdadr and cddr
             );
@@ -167,7 +274,31 @@ scheme.definition_value = function (exp) {
     }  
 }
 
+// ¤ Lambda expressions are lists that begin with the symbol lambda:
 
+// (define (lambda? exp) (tagged-list? exp 'lambda))
+// (define (lambda-parameters exp) (cadr exp))
+// (define (lambda-body exp) (cddr exp))
+
+// We also provide a constructor for lambda expressions, which is used by definition-value, above:
+
+// (define (make-lambda parameters body)
+//   (cons 'lambda (cons parameters body)))
+
+// ¤ Conditionals begin with if and have a predicate, a consequent, and an (optional) alternative. If the expression has no alternative part, we provide false as the alternative.10
+
+// (define (if? exp) (tagged-list? exp 'if))
+// (define (if-predicate exp) (cadr exp))
+// (define (if-consequent exp) (caddr exp))
+// (define (if-alternative exp)
+//   (if (not (null? (cdddr exp)))
+//       (cadddr exp)
+//       'false))
+
+// We also provide a constructor for if expressions, to be used by cond->if to transform cond expressions into if expressions:
+
+// (define (make-if predicate consequent alternative)
+//   (list 'if predicate consequent alternative))
 
 
 
@@ -270,3 +401,10 @@ scheme.ApplyException = function (msg, proc) {
     this.procedure = proc;
 };
 scheme.ApplyException.prototype = new scheme.Exception();
+
+scheme.ParseException = function (msg, exp) {
+    this.name = 'ParseException';
+    this.message = msg;
+    this.expression = exp;
+};
+scheme.EvalException.prototype = new scheme.Exception();
